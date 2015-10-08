565b85d3bc9183a2364c075b76349e848cf18b19
Wanderley Panosso <wanderley_panosso@hotmail.com>
2015-10-08 11:34:16 -0300

    readme

--- client/app.js
+++ client/app.js
@@ -2,10 +2,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//var fileManager = require('express-file-manager');
-
-//app.use('/filemanager', fileManager(__dirname + '/public', {}));
-
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.listen(3000);
